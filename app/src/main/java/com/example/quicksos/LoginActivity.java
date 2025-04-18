e690ed2fa30127ace6007e631475285b1bac77c1
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2025-04-18 12:04:58 +0200

    Login activity finished

--- app/src/main/java/com/example/quicksos/LoginActivity.java
+++ app/src/main/java/com/example/quicksos/LoginActivity.java
@@ -1,19 +1,27 @@
 package com.example.quicksos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Iniciate Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         // Obtain the views references
         TextView welcomeTextView = findViewById(R.id.welcomeTextView);
@@ -33,5 +41,29 @@ public class LoginActivity extends AppCompatActivity {
         questionTextView.setContentDescription("¿No tienes una cuenta?");
         registerButton.setContentDescription("Crear cuenta");
 
+        // Buttons click configuration
+        loginButton.setOnClickListener(v -> {
+            String email = mailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            loginUser(email, password);
+        });
+
+        registerButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+//            startActivity(intent);
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error en autenticación.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
